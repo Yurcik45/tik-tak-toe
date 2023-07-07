f4930f14156d60c7180c1e158d597737975be4e6
yurcik45 <yurcik45mail@gmail.com>
2023-07-07 15:01:22 +0300

    add ws connection example

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -3,9 +3,15 @@ import { SquareArea } from './components/SquareArea'
 
 export const App = () =>
 {
+  const myWs = new WebSocket('ws://localhost:9000')
+  myWs.onopen = () => { console.log('connected') }
+  myWs.onmessage = (message) => { console.log('Message: ', message) }
+  const wsSend = () => { console.log("trying to send the message"); myWs.send(JSON.stringify("message from website")) }
+
   return (
     <div className="App">
       <SquareArea />
+      <button onClick={ wsSend }>send ws msg</button>
     </div>
   )
 }
