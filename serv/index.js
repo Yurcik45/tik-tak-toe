4284b101267feb15a0645497800eab686744f675
yurcik45 <yurcik45mail@gmail.com>
2023-07-07 14:59:33 +0300

    remove express and cors packages, add normal ws initial connection handler

--- serv/index.js
+++ serv/index.js
@@ -1,10 +1,18 @@
-const express = require("express")
-const cors = require("cors")
+const WS = require("ws")
 require('dotenv').config()
 
-const PORT = process.env['PORT']
-const app = express()
+const port = process.env['PORT']
 
-app.use(cors())
+const wss = new WS.Server({ port }, () => console.log(`SERV: websocket started on port: ${port}`))
 
-app.listen(PORT, () => console.log(`server has been started on port ${PORT}`))
+wss.on('connection', (ws, req) => {
+  const raw_headers = req.rawHeaders
+  const origin = raw_headers[raw_headers.indexOf('Origin') + 1]
+  console.log("request from: ", origin)
+  ws.isAlive = true
+  ws.on('error', console.error)
+  console.log("new ws user", ws)
+  ws.send("hello, new user")
+  ws.on('message', msg => console.log("message from user: ", JSON.parse(msg)))
+  ws.on('close', () => console.log("user disconnected"))
+})
