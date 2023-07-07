951051fac288873c54b14fd95018fb8390550070
yurcik45 <yurcik45mail@gmail.com>
2023-07-07 16:52:22 +0300

    make game more 'imaginable', add styles and concept

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,17 +1,26 @@
+import { useState } from 'react'
 import './App.css'
+import { InfoScreen } from './components/InfoScreen'
 import { SquareArea } from './components/SquareArea'
 
 export const App = () =>
 {
-  const myWs = new WebSocket('ws://localhost:9000')
-  myWs.onopen = () => { console.log('connected') }
-  myWs.onmessage = (message) => { console.log('Message: ', message) }
-  const wsSend = () => { console.log("trying to send the message"); myWs.send(JSON.stringify("message from website")) }
+  const [game_part, set_game_part] = useState("init") // "start" "finish" "error"
 
   return (
     <div className="App">
-      <SquareArea />
-      <button onClick={ wsSend }>send ws msg</button>
+      { game_part !== "start" && <InfoScreen
+        label={ game_part === "error" ? "something went wrong :(" : game_part == "init" ? "tic tac toe online" : "one more time?" }
+        action={() => { game_part === "error" ? window.navigation.reload() : set_game_part("start") }}
+        action_label={ game_part === "error" ? "reset game" : game_part == "init" ? "start game" : "restart game" }
+        background={ game_part === "error" ? "bg2" : game_part === "init" ? "bg1" : null }
+      /> }
+      { game_part === "start" && <SquareArea set_game_part={ set_game_part } /> }
     </div>
   )
 }
+
+// const myWs = new WebSocket('ws://localhost:9000')
+// myWs.onopen = () => { console.log('connected') }
+// myWs.onmessage = (message) => { console.log('Message: ', message) }
+// const wsSend = () => { console.log("trying to send the message"); myWs.send(JSON.stringify("message from website")) }
--- tic-tac-toe/src/components/SquareArea/index.jsx
+++ tic-tac-toe/src/components/SquareArea/index.jsx
@@ -2,19 +2,9 @@ import { useEffect, useState } from 'react'
 import { SquareItem } from '../SquareItem'
 import s from './index.module.css'
 
-export const SquareArea = () =>
+export const SquareArea = ({ set_game_part }) =>
 {
-  const items_initial = Array(9).fill({ symbol: null, acrossed: false })
-  const [items, set_items] = useState(items_initial)
-  const [is_finish, set_is_finish] = useState(false)
-
-  // [ { symbol: null, acrossed: false }, { symbol: null, acrossed: false }, { symbol: null, acrossed: false },
-  //   { symbol: null, acrossed: false }, { symbol: null, acrossed: false }, { symbol: null, acrossed: false },
-  //   { symbol: null, acrossed: false }, { symbol: null, acrossed: false }, { symbol: null, acrossed: false }, ]
-
-  // 0  1  2
-  // 3  4  5
-  // 6  7  8
+  const [items, set_items] = useState(Array(9).fill({ symbol: null, acrossed: false }))
 
   const item_click = (item_id, is_symbol) =>
   {
@@ -22,47 +12,43 @@ export const SquareArea = () =>
     set_items(items.map((item, id) => item_id === id ? ({ ...item, symbol: "x" }) : item ))
   }
 
-  const check_winner = (symbol) =>
+  const check_winner = () =>
   {
     // horizontal
-    if (items[0].symbol === symbol && items[1].symbol === symbol && items[2].symbol === symbol ) return { status: true, ids: [0,1,2], mode: "horizontal" }
-    if (items[3].symbol === symbol && items[4].symbol === symbol && items[5].symbol === symbol ) return { status: true, ids: [3,4,5], mode: "horizontal" }
-    if (items[6].symbol === symbol && items[7].symbol === symbol && items[8].symbol === symbol ) return { status: true, ids: [6,7,8], mode: "horizontal" }
+    if (items[1].symbol !== null && items[0].symbol === items[1].symbol && items[1].symbol === items[2].symbol ) return { status: true, ids: [0,1,2], mode: "horizontal" }
+    if (items[4].symbol !== null && items[3].symbol === items[4].symbol && items[4].symbol === items[5].symbol ) return { status: true, ids: [3,4,5], mode: "horizontal" }
+    if (items[7].symbol !== null && items[6].symbol === items[7].symbol && items[7].symbol === items[8].symbol ) return { status: true, ids: [6,7,8], mode: "horizontal" }
     // vertical
-    if (items[0].symbol === symbol && items[3].symbol === symbol && items[6].symbol === symbol ) return { status: true, ids: [0,3,6], mode: "vertical" }
-    if (items[1].symbol === symbol && items[4].symbol === symbol && items[7].symbol === symbol ) return { status: true, ids: [1,4,7], mode: "vertical" }
-    if (items[2].symbol === symbol && items[5].symbol === symbol && items[8].symbol === symbol ) return { status: true, ids: [2,5,8], mode: "vertical" }
+    if (items[3].symbol !== null && items[0].symbol === items[3].symbol && items[3].symbol === items[6].symbol ) return { status: true, ids: [0,3,6], mode: "vertical" }
+    if (items[4].symbol !== null && items[1].symbol === items[4].symbol && items[4].symbol === items[7].symbol ) return { status: true, ids: [1,4,7], mode: "vertical" }
+    if (items[5].symbol !== null && items[2].symbol === items[5].symbol && items[5].symbol === items[8].symbol ) return { status: true, ids: [2,5,8], mode: "vertical" }
     // across
-    if (items[0].symbol === symbol && items[4].symbol === symbol && items[8].symbol === symbol ) return { status: true, ids: [0,4,8], mode: "across" }
-    if (items[2].symbol === symbol && items[4].symbol === symbol && items[6].symbol === symbol ) return { status: true, ids: [1,4,7], mode: "across_reverse" }
+    if (items[4].symbol !== null && items[0].symbol === items[4].symbol && items[4].symbol === items[8].symbol ) return { status: true, ids: [0,4,8], mode: "across" }
+    if (items[4].symbol !== null && items[2].symbol === items[4].symbol && items[4].symbol === items[6].symbol ) return { status: true, ids: [2,4,6], mode: "across_reverse" }
     return { status: false, ids: [], mode: null }
   }
 
   const check_acrossed = () =>
   {
     const steps_count = items.filter(item => item.symbol !== null).length
-    if (steps_count < 5) return console.log("too early for find the winner")
-    console.log("find the winner logic ..")
-    const result = check_winner("x") 
-    console.log("result: ", result)
-    if (result.status) set_is_finish(true)
+    if (steps_count < 5) return
+    const result = check_winner()
+    if (result.status)
+    {
+      console.log("winner is: ", items[result.ids[0]].symbol)
+      set_game_part("finish")
+    }
   }
-
-  const restart_game = () =>
-  {
-    set_is_finish(false)
-  }
-
-  console.log("items", items)
 
   useEffect(check_acrossed, [items])
 
   return (
-    <div className={ s.area }>
-      { !is_finish
-          ? items.map((item, id) => <SquareItem  key={id} { ...{ id, ...item } } onClick={ item_click } />)
-          : <div onClick={ restart_game }>One more time?</div>
-        }
+    <div className={ s.screen }>
+      <div className={ s.label }>tic tac toe</div>   
+      <div className={ s.area }>
+        { items.map((item, id) => <SquareItem  key={id} { ...{ id, ...item } } onClick={ item_click } />) }
+      </div>
+      <button className={ s.button } onClick={() => { set_game_part("init") }}>exit</button>
     </div>
   )
 }
