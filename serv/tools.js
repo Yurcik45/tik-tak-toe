3c5721e4e05e962763599fe47173e24d02f5917f
yurcik45 <yurcik45mail@gmail.com>
2023-07-13 15:54:53 +0300

    update create query and add restart query

--- serv/tools.js
+++ serv/tools.js
@@ -22,8 +22,8 @@ const game_query_builder = ({...params}) =>
   get_all: 'SELECT * FROM battles',
   get_one: `SELECT * FROM battles WHERE id=${params.id}`,
   create: `
-    INSERT INTO battles (player1_name, game_status)
-    VALUES ('${params.player1_name}', 'search opponent')
+    INSERT INTO battles (player1_name, game_status, last_step_player, player1_symbol, player2_symbol)
+    VALUES ('${params.player1_name}', 'search opponent', '2', 'x', 'o')
     RETURNING *
   `,
   start: `
@@ -40,7 +40,26 @@ const game_query_builder = ({...params}) =>
   `,
   update_data: `
     UPDATE battles
-    SET game_data='${JSON.stringify(params.game_data)}'
+    SET game_data='${JSON.stringify(params.game_data)}', last_step_player='${params.last_step_player}'
+    WHERE id=${params.id}
+    RETURNING *
+  `,
+  restart: `
+    UPDATE battles
+    SET
+      is_player1_online=${params.is_player1_online},
+      is_player2_online=${params.is_player2_online},
+      game_status='running', game_data: '[
+        {"id": 0,"symbol": null, "acrossed": false},
+        {"id": 1,"symbol": null, "acrossed": false},
+        {"id": 2,"symbol": null, "acrossed": false},
+        {"id": 3,"symbol": null, "acrossed": false},
+        {"id": 4,"symbol": null, "acrossed": false},
+        {"id": 5,"symbol": null, "acrossed": false},
+        {"id": 6,"symbol": null, "acrossed": false},
+        {"id": 7,"symbol": null, "acrossed": false},
+        {"id": 8,"symbol": null, "acrossed": false}
+      ]'
     WHERE id=${params.id}
     RETURNING *
   `,
