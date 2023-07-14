de98eaf58b53d29b8928a5d551f588725a736deb
yurcik45 <yurcik45mail@gmail.com>
2023-07-14 15:28:55 +0300

    view new x_o images in SquareItem component and its across variants

--- tic-tac-toe/src/components/SquareItem/index.jsx
+++ tic-tac-toe/src/components/SquareItem/index.jsx
@@ -1,9 +1,16 @@
+import x_item from '../../assets/game/x.svg'
+import zero_icon from '../../assets/game/zero.svg'
+import x_cross_item from '../../assets/game/x_cross.svg'
+import zero_cross_icon from '../../assets/game/zero_cross.svg'
 import s from './index.module.css'
 
 export const SquareItem = ({ id, symbol, acrossed, onClick }) =>
   <div
-    className={ `${ s.item }` + " " + `${ acrossed ? s.acrossed : "" }` }
+    className={`
+      ${ s.item }
+      ${ symbol === null ? s.false_hover : s.true_hover }
+    `}
     onClick={ () => onClick(id, symbol !== null) }
   >
-    { symbol }
+   { symbol && <img src={ acrossed ? symbol === "o" ? zero_cross_icon : x_cross_item : symbol === "o" ? zero_icon : x_item } /> }
   </div>
