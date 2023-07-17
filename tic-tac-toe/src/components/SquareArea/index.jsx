837a5ab8475216da9992d717a1c4f97d2fad96d5
yurcik45 <yurcik45mail@gmail.com>
2023-07-17 17:51:11 +0300

    add send_finish_game and notify functions; update item_click method; update check_winner method

--- tic-tac-toe/src/components/SquareArea/index.jsx
+++ tic-tac-toe/src/components/SquareArea/index.jsx
@@ -1,55 +1,52 @@
 import { v4 as uuid } from 'uuid'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { SquareItem } from '../SquareItem'
 import s from './index.module.css'
 
-export const SquareArea = ({ battle_data, set_game_part, make_step, symbol }) =>
+export const SquareArea = ({ game_data, set_game_part, make_step, symbol, send_finish_game, notify }) =>
 {
   const item_click = (item_id, is_symbol) =>
   {
-    if (is_symbol && battle_data[item_id].symbol !== symbol) return console.log("That's you opponent square!")
-    if (is_symbol && battle_data[item_id].symbol === symbol) return console.log("You can't select it twice")
-    const updated_items = battle_data.map((item, id) => item_id === id ? ({ ...item, symbol }) : item )
-    // set_items(updated_items)
-    // console.log({ symbol, item_id, updated_items })
-    make_step(updated_items)
+    if (is_symbol && game_data[item_id].symbol !== symbol) return notify("warning", "That's you opponent square!")
+    if (is_symbol && game_data[item_id].symbol === symbol) return notify("info", "You can't select it twice")
+    make_step(game_data.map((item, id) => item_id === id ? ({ ...item, symbol }) : item ))
   }
 
   const check_winner = () =>
   {
     // horizontal
-    if (battle_data[1].symbol !== null && battle_data[0].symbol === battle_data[1].symbol && battle_data[1].symbol === battle_data[2].symbol ) return { status: true, ids: [0,1,2], mode: "horizontal" }
-    if (battle_data[4].symbol !== null && battle_data[3].symbol === battle_data[4].symbol && battle_data[4].symbol === battle_data[5].symbol ) return { status: true, ids: [3,4,5], mode: "horizontal" }
-    if (battle_data[7].symbol !== null && battle_data[6].symbol === battle_data[7].symbol && battle_data[7].symbol === battle_data[8].symbol ) return { status: true, ids: [6,7,8], mode: "horizontal" }
+    if (game_data[1].symbol !== null && game_data[0].symbol === game_data[1].symbol && game_data[1].symbol === game_data[2].symbol ) return [0,1,2]
+    if (game_data[4].symbol !== null && game_data[3].symbol === game_data[4].symbol && game_data[4].symbol === game_data[5].symbol ) return [3,4,5]
+    if (game_data[7].symbol !== null && game_data[6].symbol === game_data[7].symbol && game_data[7].symbol === game_data[8].symbol ) return [6,7,8]
     // vertical
-    if (battle_data[3].symbol !== null && battle_data[0].symbol === battle_data[3].symbol && battle_data[3].symbol === battle_data[6].symbol ) return { status: true, ids: [0,3,6], mode: "vertical" }
-    if (battle_data[4].symbol !== null && battle_data[1].symbol === battle_data[4].symbol && battle_data[4].symbol === battle_data[7].symbol ) return { status: true, ids: [1,4,7], mode: "vertical" }
-    if (battle_data[5].symbol !== null && battle_data[2].symbol === battle_data[5].symbol && battle_data[5].symbol === battle_data[8].symbol ) return { status: true, ids: [2,5,8], mode: "vertical" }
+    if (game_data[3].symbol !== null && game_data[0].symbol === game_data[3].symbol && game_data[3].symbol === game_data[6].symbol ) return [0,3,6]
+    if (game_data[4].symbol !== null && game_data[1].symbol === game_data[4].symbol && game_data[4].symbol === game_data[7].symbol ) return [1,4,7]
+    if (game_data[5].symbol !== null && game_data[2].symbol === game_data[5].symbol && game_data[5].symbol === game_data[8].symbol ) return [2,5,8]
     // across
-    if (battle_data[4].symbol !== null && battle_data[0].symbol === battle_data[4].symbol && battle_data[4].symbol === battle_data[8].symbol ) return { status: true, ids: [0,4,8], mode: "across" }
-    if (battle_data[4].symbol !== null && battle_data[2].symbol === battle_data[4].symbol && battle_data[4].symbol === battle_data[6].symbol ) return { status: true, ids: [2,4,6], mode: "across_reverse" }
-    return { status: false, ids: [], mode: null }
+    if (game_data[4].symbol !== null && game_data[0].symbol === game_data[4].symbol && game_data[4].symbol === game_data[8].symbol ) return [0,4,8]
+    if (game_data[4].symbol !== null && game_data[2].symbol === game_data[4].symbol && game_data[4].symbol === game_data[6].symbol ) return [2,4,6]
+    return []
   }
 
   const check_acrossed = () =>
   {
-    const steps_count = battle_data.filter(item => item.symbol !== null).length
+    const steps_count = game_data.filter(item => item.symbol !== null).length
     if (steps_count < 5) return
-    const result = check_winner()
-    if (result.status)
-    {
-      console.log("winner is: ", battle_data[result.ids[0]].symbol)
-      set_game_part("finish")
-    }
+    const winner_ids = check_winner()
+    if (winner_ids.length === 0) return
+    notify("success", "winner is: ", game_data[winner_ids[0]].symbol)
+    const battle_accrossed = game_data.map(battle => winner_ids.indexOf(battle.id) !== -1 ? ({ ...battle, accrossed: true }) : battle)
+    send_finish_game(battle_accrossed)
+    // set_game_part("finish")
   }
 
-  useEffect(check_acrossed, [battle_data])
+  useEffect(check_acrossed, [game_data])
 
   return (
     <div className={ s.screen }>
       <div className={ s.label }>tic tac toe</div>   
       <div className={ s.area }>
-        { battle_data.map((item, id) => <SquareItem  key={ uuid() } id={ id } { ...item } onClick={ item_click } />) }
+        { game_data.map((item, id) => <SquareItem  key={ uuid() } id={ id } { ...item } onClick={ item_click } />) }
       </div>
       <button className={ s.button } onClick={() => { set_game_part("init") }}>exit</button>
     </div>
