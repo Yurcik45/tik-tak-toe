e3c2ee000b30d8946883d045903bddcc07baa698
yurcik45 <yurcik45mail@gmail.com>
2023-07-17 17:59:49 +0300

    HUGE UPDATE; rewrite all methods to communicate with websocket; add auto game deleting, add middleware to make logs and check queries execution success; second real worked version

--- serv/index.js
+++ serv/index.js
@@ -1,98 +1,153 @@
-const WS = require("ws")
-require('dotenv').config()
-const { db_init, make_query } = require('./db_queries')
-const { log_battle_rows } = require('./tools')
-const express = require('express')
-const cors = require('cors')
+const WS = require("ws");
+require("dotenv").config();
+const { db_init, make_query } = require("./db_queries");
+const { log_battle_rows } = require("./tools");
+const express = require("express");
+const cors = require("cors");
 
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
 
-app.get('/battles', async (req, res) =>
-{
-  const data = await make_query()
-  console.log("getting battles API result:")
-  log_battle_rows(data)
-  res.status(200).json(data.rows)
-})
+app.get("/battles", async (req, res) => {
+  const data = await make_query();
+  console.log("getting battles API result:");
+  log_battle_rows(data);
+  res.status(200).json(data.rows);
+});
 
-app.listen(5000, () => console.log("serv started on port 5000"))
+app.listen(5000, "192.168.88.76", () =>
+  console.log("serv started on port 5000")
+);
 
-const init = async () => await db_init()
+const init = async () => await db_init();
 
-const port = process.env['PORT']
+const port = process.env["PORT"];
 
-const wss = new WS.Server({ port }, () => console.log(`SERV: websocket started on port: ${port}`))
+const wss = new WS.Server({ port }, () =>
+  console.log(`SERV: websocket started on port: ${port}`)
+);
 
-const ws_message_handler = (ws, name, message) =>
-{
-  console.log("===== ws_message_handler =====")
-  console.log({name, message})
-  console.log("===== ===== ===== ===== =====")
-  switch (message.title) {
-    case "start":
-      make_query("create", { player1_name: name })
-      .then(() =>
-      {
-        ws.send(JSON.stringify({ title: "created", msg: "battle was created" }))
-        console.log("after sending msg in case start")
-      })
-      break
-    case "step":
-      console.log("STEP CASE", message)
-      wss.clients.forEach(client => console.log("client origin: ", client.origin ))
-      make_query("get_one", { id: message.battle_id })
-      .then(battles =>
-      {
-        console.log("battles when get in step")
-        log_battle_rows(battles)
-        make_query("update_data", { id: message.battle_id, game_data: message.data, last_step_player: name })
-        .then(updated_battle =>
-        {
-          console.log("updated battle in step case: ")
-          log_battle_rows(updated_battle)
-          wss.clients.forEach(client =>
-          {
-            const data = updated_battle.rows[0]
-            if (data.player1_name === client.origin || data.player2_name === client.origin) client.send(JSON.stringify({ title: "step", battle: updated_battle.rows[0] }))
-          })
-        })
-      })
-      break
-    case "restart":
-      
-      break
-    case "connect":
-      const { id, player2_name } = message
-      make_query("start", { id, player2_name })
-      .then(result =>
-      {
-        console.log("connect db results ====:")
-        log_battle_rows(result)
-        ws.send(JSON.stringify({ title: "connect", battle: result.rows[0] }))
-      })
-      break
-    case "test":
-      console.log("test message from ", name)
-      ws.send(JSON.stringify({ title: "test", msg: "teeest" }))
-      console.log("after sending msg in case test")
-      break
-    default: break
+const check_rows_case = (res, message) => {
+  console.log(`====== ${message.title} case`);
+  if (res.rows.length === 0) {
+    console.log("FAILED\n", { res, message });
+    return false;
   }
-}
+  log_battle_rows(res);
+  console.log("==============================");
+  return true;
+};
 
-wss.on('connection', (ws, req) => {
-  const raw_headers = req.rawHeaders
-  const origin = raw_headers[raw_headers.indexOf('Origin') + 1]
-  console.log("request from: ", origin)
-  ws.isAlive = true
-  ws.origin = origin
-  ws.on('error', console.error)
-  ws.send(JSON.stringify({ title: "hello", msg: `hello, ${origin}!` }))
-  ws.on('message', msg => ws_message_handler(ws, origin, JSON.parse(msg)))
-  ws.on('close', () => console.log("user disconnected"))
-})
+const ws_message_handler = (ws, name, message) => {
+  console.log("===== ws_message_handler =====");
+  console.log({ name, message });
+  console.log("===== ===== ===== ===== =====");
+  const { title } = message;
+  switch (title) {
+    case "create_game":
+      make_query("create", { player1_name: name }).then((res) => {
+        if (!check_rows_case(res, message)) return;
+        wss.clients.forEach((client) =>
+          client.send(
+            JSON.stringify({
+              title: "game_created",
+              msg: "battle was created",
+              battle_data: res.rows[0],
+            })
+          )
+        );
+        console.log("after sending msg in case start");
+      });
+      break;
+    case "connect_to_battle":
+      make_query("start", {
+        id: message.id,
+        player2_name: message.player2_name,
+      }).then((res) => {
+        if (!check_rows_case(res, message)) return;
+        wss.clients.forEach((client) => {
+          const ws_origin = client.origin;
+          const battle_data = res.rows[0];
+          const { player1_name, player2_name } = battle_data;
+          console.log({ ws_origin, player1_name, player2_name });
+          if (player1_name === ws_origin || player2_name === ws_origin) {
+            client.send(
+              JSON.stringify({
+                title: "game_started",
+                battle_data: res.rows[0],
+                new_user_name: player2_name,
+              })
+            );
+          }
+        });
+      });
+      break;
+    case "step":
+      console.log("STEP CASE", message);
+      make_query("get_one", { id: message.battle_data.id }).then((res) => {
+        if (!check_rows_case(res, message)) return;
+        make_query("update_data", {
+          id: message.battle_data.id,
+          game_data: message.game_data,
+          last_step_player: message.battle_data.player1_name === message.battle_data.last_step_player ? message.battle_data.player2_name : message.battle_data.player1_name,
+        }).then((updated_battle) => {
+          console.log("updated battle in step case: ");
+          if (!check_rows_case(updated_battle, message)) return;
+          wss.clients.forEach((client) => {
+            const ws_origin = client.origin;
+            const battle_data = updated_battle.rows[0];
+            const { player1_name, player2_name } = battle_data;
+            if (player1_name === ws_origin || player2_name === ws_origin)
+              client.send(
+                JSON.stringify({ title: "step", battle: battle_data })
+              );
+          });
+        });
+      });
+      break;
+    case "finish_game":
+      const { id, game_data } = message;
+      make_query("finish", { id, game_data }).then((res) => {
+        if (!check_rows_case(res, message)) return;
+        console.log("game finished OK");
+      });
+    default:
+      break;
+  }
+};
 
+wss.on("connection", (ws, req) => {
+  const raw_headers = req.rawHeaders;
+  const origin = raw_headers[raw_headers.indexOf("Origin") + 1];
+  console.log("request from: ", origin);
+  ws.isAlive = true;
+  ws.origin = origin;
+  ws.on("error", console.error);
+  ws.send(JSON.stringify({ title: "hello", msg: `hello, ${origin}!` }));
+  ws.on("message", (msg) => ws_message_handler(ws, origin, JSON.parse(msg)));
+  ws.on("close", () => {
+    // get battle via on eof players name
+    make_query("get_one_by_name", { name: origin }).then((res) => {
+      if (!check_rows_case(res, { title: "general get_one_by_name" })) return;
+      make_query("delete", { id: res.rows[0].id }).then((del_res) => {
+        if (!check_rows_case(del_res, { title: "general delete" })) return;
+        wss.clients.forEach((client) => {
+          const ws_origin = client.origin;
+          const battle_data = del_res.rows[0];
+          const { player1_name, player2_name } = battle_data;
+          if (player1_name === ws_origin || player2_name === ws_origin) {
+            client.send(
+              JSON.stringify({
+                title: "leave",
+                msg: `player ${ws_origin} is leave, game is finished, you win!`,
+              })
+            );
+          }
+        });
+      });
+    });
+    console.log("user disconnected");
+  });
+});
 
-
-init()
+init();
